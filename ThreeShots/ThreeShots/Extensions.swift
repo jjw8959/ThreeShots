04a776706c85aa6f150804c5c8c6455293b46f80
JaeWoongJeong <jjw8959@naver.com>
2024-06-24 11:00:15 +0900

    feat: Date <-> String을 위해 extension추가
    테스트와 실사용을 위해 Date와 String에 extension으로
    각각 toString(), toDate() 메서드를 추가함

---
+++ ThreeShots/ThreeShots/Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  Extensions.swift
+//  ThreeShots
+//
+//  Created by woong on 6/20/24.
+//
+
+import Foundation
+import UIKit
+
+extension Date {
+    func toString() -> String {
+        let df = DateFormatter()
+        df.dateFormat = "yy MM dd"
+        df.timeZone = TimeZone(identifier: "UTC")
+        return df.string(from: self)
+    }
+}
+
+extension String {
+    func toDate() -> Date? {
+        let df = DateFormatter()
+        df.dateFormat = "yy MM dd"
+        df.timeZone = TimeZone(identifier: "UTC")
+        if let date = df.date(from: self) {
+            return date
+        } else {
+            return nil
+        }
+    
+    }
+}
