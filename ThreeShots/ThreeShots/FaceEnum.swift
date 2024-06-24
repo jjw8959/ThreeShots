0a07427670bc085351019353fb85c552ef48ff2c
JaeWoongJeong <jjw8959@naver.com>
2024-06-24 10:58:27 +0900

    feat: 표정 이모지를 위한 enum 추가

---
+++ ThreeShots/ThreeShots/FaceEnum.swift
@@ -0,0 +1,16 @@
+//
+//  FaceEnum.swift
+//  ThreeShots
+//
+//  Created by woong on 6/20/24.
+//
+
+import Foundation
+
+enum FaceEnum: String, CaseIterable {
+    case happy
+    case smile
+    case sad
+    case bad
+    case soso
+}
