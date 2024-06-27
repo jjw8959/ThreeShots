2386985ca0140ff710ea9981d422525d2266efbf
JaeWoongJeong <jjw8959@naver.com>
2024-06-27 11:29:56 +0900

    feat: DetailView 구현 완료

--- ThreeShots/ThreeShots/Woong/View/DailyView/SubView/PhotoView.swift
+++ ThreeShots/ThreeShots/Woong/View/DailyView/SubView/PhotoView.swift
@@ -9,46 +9,36 @@ import UIKit
 
 final class PhotoView: UIView {
     
-    private let firstImageView: UIImageView = {
+    let firstImageView: UIImageView = {
         let view = UIImageView()
-        view.image = UIImage(systemName: "sun.max")
-        view.contentMode = .scaleAspectFit
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    private let secondImageView: UIImageView = {
-        let view = UIImageView()
-        view.image = UIImage(systemName: "sun.max")
-        view.contentMode = .scaleAspectFit
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    private let thirdImageView: UIImageView = {
-        let view = UIImageView()
-        view.image = UIImage(systemName: "sun.max")
-        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: "mock1")
+        view.clipsToBounds = true
+        view.contentMode = .scaleAspectFill
+        view.layer.cornerRadius = 10
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    public var firstImage: UIImage? {
-        didSet {
-            firstImageView.image = firstImage
-        }
-    }
+    let secondImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "mock2")
+        view.clipsToBounds = true
+        view.contentMode = .scaleAspectFill
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
-    public var secondImage: UIImage? {
-        didSet {
-            secondImageView.image = secondImage
-        }
-    }
+    let thirdImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "mock3")
+        view.clipsToBounds = true
+        view.contentMode = .scaleAspectFill
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
-    public var thirdImage: UIImage? {
-        didSet {
-            thirdImageView.image = thirdImage
-        }
-    }
-       
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubviews()
@@ -60,9 +50,11 @@ final class PhotoView: UIView {
     }
     
     private func addSubviews() {
+        
         self.addSubview(firstImageView)
         self.addSubview(secondImageView)
         self.addSubview(thirdImageView)
+        
     }
     
     private func addConstraints() {
@@ -75,12 +67,49 @@ final class PhotoView: UIView {
             secondImageView.topAnchor.constraint(equalTo: self.topAnchor),
             secondImageView.leadingAnchor.constraint(equalTo: firstImageView.trailingAnchor),
             secondImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            secondImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
             secondImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5),
             
             thirdImageView.topAnchor.constraint(equalTo: secondImageView.bottomAnchor),
-            thirdImageView.leadingAnchor.constraint(equalTo: firstImageView.trailingAnchor),
+            thirdImageView.leadingAnchor.constraint(equalTo: secondImageView.leadingAnchor),
             thirdImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            thirdImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
             thirdImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5),
         ])
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        if firstImageView.frame.contains(location) {
+            firstImageViewTapped()
+        }
+        
+        if secondImageView.frame.contains(location) {
+            secondImageViewTapped()
+        }
+        
+        if thirdImageView.frame.contains(location) {
+            thirdImageViewTapped()
+        }
+    }
+    
+    private func firstImageViewTapped() {
+        print("firstImageView Tapped in PhotoView")
+    }
+    
+    private func secondImageViewTapped() {
+        print("secondImageViewTapped Tapped in PhotoView")
+    }
+    
+    private func thirdImageViewTapped() {
+        print("secondImageViewTapped Tapped in PhotoView")
+    }
+}
+
+#Preview {
+    PhotoView()
 }
