8029e1778966730f30b2f78c1397b7433128ea11
JaeWoongJeong <jjw8959@naver.com>
2024-07-25 10:34:23 +0900

    fix: timezone 때문에 날짜 잘못나와서 timezone제거

--- ThreeShots/ThreeShots/Extensions.swift
+++ ThreeShots/ThreeShots/Extensions.swift
@@ -12,13 +12,11 @@ extension Date {
     func toString() -> String {
         let df = DateFormatter()
         df.dateFormat = "yy.MM.dd"
-        df.timeZone = TimeZone(identifier: "UTC")
         return df.string(from: self)
     }
     func toStringDate() -> String {
         let df = DateFormatter()
         df.dateFormat = "dd일"
-        df.timeZone = TimeZone(identifier: "UTC")
         return df.string(from: self)
     }
 }
@@ -27,7 +25,6 @@ extension String {
     func toDate() -> Date? {
         let df = DateFormatter()
         df.dateFormat = "yy MM dd"
-        df.timeZone = TimeZone(identifier: "UTC")
         if let date = df.date(from: self) {
             return date
         } else {
