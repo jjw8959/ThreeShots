e516b25766cc0ccb4e94176e18e31438b5190ffc
JaeWoongJeong <jjw8959@naver.com>
2024-07-29 04:23:04 +0900

    fix: 파일 위치 변경

---
+++ ThreeShots/ThreeShots/App/Diary+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Diary+CoreDataClass.swift
+//  ThreeShots
+//
+//  Created by woong on 7/29/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Diary)
+public class Diary: NSManagedObject {
+
+}
