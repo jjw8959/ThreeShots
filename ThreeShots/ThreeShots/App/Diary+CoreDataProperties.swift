4c7914ca86ec36540c776ce0bd2065b517071c4b
JaeWoongJeong <jjw8959@naver.com>
2024-07-29 15:14:45 +0900

    feat: 파일명 교체 및 Enums는 쓰지않아 삭제

--- ThreeShots/ThreeShots/App/Diary+CoreDataProperties.swift
+++ ThreeShots/ThreeShots/App/Diary+CoreDataProperties.swift
@@ -15,13 +15,14 @@ extension Diary {
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Diary> {
         return NSFetchRequest<Diary>(entityName: "Diary")
     }
-
+    @NSManaged public var year: String?
     @NSManaged public var date: String?
     @NSManaged public var content: String?
     @NSManaged public var firstImage: String?
     @NSManaged public var secondImage: String?
     @NSManaged public var thirdImage: String?
     @NSManaged public var id: UUID?
+    @NSManaged public var month: String?
 
 }
 
