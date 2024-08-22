c1e26191a5417cd4c43b4e5d99849ea36322fcf1
JaeWoong Jeong <jjw8959@naver.com>
2024-08-22 18:44:23 +0900

    feat: DailyView 구현 완료

--- ThreePicDiary/ThreePicDiary/View/ChildView/DailyView.swift
+++ ThreePicDiary/ThreePicDiary/View/ChildView/DailyView.swift
@@ -6,10 +6,67 @@
 //
 
 import UIKit
+import CoreData
 
 final class DailyView: UIViewController {
     
+    let coredata = DataManager.shared
+    
+    let tableView = UITableView()
+    
+    var diarys: [Diary]?
+    
+    let cellName = "DailyViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(DailyViewCell.self, forCellReuseIdentifier: cellName)
+        
+        diarys = coredata.loadDailyData()
+        
+        setView()
+    }
+    
+    private func setView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+}
+
+extension DailyView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if diarys == nil {
+            return 0
+        } else {
+            return diarys!.count
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath) as? DailyViewCell else { return UITableViewCell() }
+        cell.dateString = diarys?[indexPath.row].date
+        cell.firstImage = diarys?[indexPath.row].firstImage
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
