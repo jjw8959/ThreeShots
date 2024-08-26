71ba61fb276f633df20a9bf76d5aa95bc53beeb9
JaeWoong Jeong <jjw8959@naver.com>
2024-08-26 12:16:37 +0900

    feat: 하단에서 끌어올려서 refresh 구현 완료

--- ThreePicDiary/ThreePicDiary/View/ChildView/DailyView.swift
+++ ThreePicDiary/ThreePicDiary/View/ChildView/DailyView.swift
@@ -16,6 +16,10 @@ final class DailyView: UIViewController {
     
     var diarys: [Diary]?
     
+    let refreshControl = UIRefreshControl()
+    
+    var isBottomRefresh = false
+    
     let cellName = "DailyViewCell"
     
     override func viewDidLoad() {
@@ -24,6 +28,8 @@ final class DailyView: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(DailyViewCell.self, forCellReuseIdentifier: cellName)
+        
+        tableView.refreshControl = refreshControl
         
         diarys = coredata.loadDailyData()
         
@@ -42,12 +48,44 @@ final class DailyView: UIViewController {
             tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
         ])
+        
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 100))
+        footerView.backgroundColor = .clear
+        tableView.tableFooterView = footerView
+        
+        // 리프레시 컨트롤 설정
+        refreshControl.tintColor = .clear
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        tableView.addSubview(refreshControl)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
     }
+    
+    @objc func handleRefresh() {
+        guard isBottomRefresh else {
+            refreshControl.endRefreshing()
+            return
+        }
+        
+        // 데이터를 다시 불러오거나 원하는 작업을 수행
+        if let temp = coredata.loadDailyData() {
+            diarys?.append(contentsOf: temp)
+        }
+        tableView.reloadData()
+        
+        print("Refreshing...")
+        
+        // 작업이 완료되면 리프레시 컨트롤 종료
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.refreshControl.endRefreshing()
+            self.isBottomRefresh = false
+        }
+    }
+    
+    
 }
 
 extension DailyView: UITableViewDelegate, UITableViewDataSource {
@@ -68,6 +106,28 @@ extension DailyView: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        // TODO: 누르면 디테일뷰로 넘기기
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let scrollViewHeight = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - scrollViewHeight {
+            if offsetY - (contentHeight - scrollViewHeight) > 50 {
+                if !refreshControl.isRefreshing {
+                    isBottomRefresh = true
+                    refreshControl.beginRefreshing()
+                    handleRefresh()
+                }
+            }
+        }
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        refreshControl.endRefreshing()
+        isBottomRefresh = false
+    }
+    
 }
